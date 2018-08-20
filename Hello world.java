b24fceeca94469734e7e33fa677a1656ed53253b
mamta <mamtathakur0399@gmail.com>
2018-08-20 16:21:38 +0530

    assignment1

---
+++ Hello world.java
@@ -0,0 +1,7 @@
+class Greet
+{ 
+public static void main(String args[])
+{
+system.out.println("hello world");
+}
+}
---
+++ Name.java
@@ -0,0 +1,14 @@
+class MemberType
+{
+static String Inst;
+String myName;
+public static void main(String args[])
+{
+     Inst="Chitkara";
+  MemberType mt=new MemberType();
+     mt.myName="mamta";
+     System.out.println("Collage" +Inst);
+       
+     System.out.println("My Name "+mt.myName);
+}
+}
