5638ac80e00e0a8d7c9d8ac02146e8f06101c597
mamta <mamtathakur0399@gmail.com>
2018-09-14 12:52:13 +0530

    dynamic data storage-1

---
+++ path.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+class WriteContent
+{
+public static void main(String[] args)throws IOException
+{
+FileInputStream in=null;
+FileOutputStream out=null;
+try
+{
+in=new FileInputStream("D:/java/abc.txt");
+out=new FileOutputStream("D:/java/xyz.txt");
+int c;
+while((c=in.read())!=-1)
+{
+out.write(c);
+}
+}
+finally
+{if(in!=null){
+in.close();
+}
+if(out!=null){
+out.close();
+}
+}
+in=new FileInputStream("D:/java/xyz.txt");
+int c;
+while((c=in.read())!=-1)
+{
+System.out.print((char)c);
+}
+}
+}
